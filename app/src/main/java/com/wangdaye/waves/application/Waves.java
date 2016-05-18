690c9a651c1f2481e854a0ba003a9da751e4d2c2
wangdaye <wangdayeeeeee@gmail.com>
2016-05-18 14:08:33 +0800

    0.0.4

--- app/src/main/java/com/wangdaye/waves/application/Waves.java
+++ app/src/main/java/com/wangdaye/waves/application/Waves.java
@@ -1,29 +1,12 @@
 package com.wangdaye.waves.application;
 
 import android.app.Application;
-import android.content.Context;
-
-import com.squareup.leakcanary.LeakCanary;
-import com.squareup.leakcanary.RefWatcher;
 
 /**
  * Waves application class.
  * */
 
 public class Waves extends Application {
-    // widget
-    private RefWatcher refWatcher;
-
     // data
     public static final int WRITE_EXTERNAL_STORAGE = 2;
-
-    @Override public void onCreate() {
-        super.onCreate();
-        LeakCanary.install(this);
-    }
-
-    public static RefWatcher getRefWatcher(Context context) {
-        Waves waves = (Waves) context.getApplicationContext();
-        return waves.refWatcher;
-    }
 }
